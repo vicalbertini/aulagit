765373df86b57f0d48b7b9eb97d929f76dbd713e
Victor Albertini <victaoalbertini@hotmail.com>
2021-03-08 21:24:33 -0300

    Emprega e TesteEmpregado funcionando

--- Empregado.java
+++ Empregado.java
@@ -42,7 +42,26 @@ public void setIdade(int idade) {
     this.idade = idade;
 }
 
+public void setSalario(double salario) {
+    this.salario = salario;
+}
+public double getSalario() {
+    return salario;
+}
 
+public void setBonus(double bonus) {
+    this.bonus = bonus;
+}
+public double getBonus() {
+    return bonus;
+}
+
+public void setComissao(double comissao) {
+    this.comissao = comissao;
+}
+public double getComissao() {
+    return comissao;
+}
 
 
 
